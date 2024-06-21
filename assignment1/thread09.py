1412f9e07c6d0b65e65fb40108fe8aa3e998e78c
[Sirirwat] <siriwat26room1@gmail.com>
2024-06-21 11:05:09 +0700

    08, 09 code

--- assignment1/thread09.py
+++ assignment1/thread09.py
@@ -11,4 +11,26 @@ class FakeDatabase:
 
     def locked_update(self, name):
         logging.info("Thread %s: starting update", name)
-        logging.debug("Thread %s about to lock ")
+        logging.debug("Thread %s about to lock", name)
+        with self._lock:
+            logging.debug("Thread %s has lock", name)
+            local_copy = self.value
+            local_copy +=1
+            time.sleep(0.1)
+            self.value = local_copy
+            logging.debug("Thread %s finishing update", name)
+
+if __name__ == "__main__" :
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level= logging.DEBUG , datefmt="%H:%M:%S")
+    #logging.getLogger().setLevel(logging.DEBUG)
+    
+    database = FakeDatabase()
+    logging.info(
+        "Testing update. Starting value is %d", database.value
+        )
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers= 2 ) as executor:
+        for index in range(2):
+            executor.submit(database.locked_update, index)
+    logging.info("Testing update. Ending value is %d", database.value)
