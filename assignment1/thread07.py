dd56a3a9f7056ff794c854c2b52952c7e5bd4317
[Sirirwat] <siriwat26room1@gmail.com>
2024-06-21 10:11:52 +0700

    assign 07 code

--- assignment1/thread07.py
+++ assignment1/thread07.py
@@ -3,4 +3,9 @@ import concurrent.futures
 import logging 
 import time 
 
-def FakeDatabase
+def thread_function(name):
+    logging.info("Thread %s; starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ = "__main__"
