3deed8f0fc21b5d814506f7e14bc6baa893103e7
[Sirirwat] <siriwat26room1@gmail.com>
2024-06-21 10:16:39 +0700

    asssign 07 fix

--- assignment1/thread07.py
+++ assignment1/thread07.py
@@ -8,4 +8,12 @@ def thread_function(name):
     time.sleep(2)
     logging.info("Thread %s: finishing", name)
 
-if __name__ = "__main__"
+if __name__ == "__main__": 
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 3) as executor:
+        executor.map(thread_function, range(3))
+
+
