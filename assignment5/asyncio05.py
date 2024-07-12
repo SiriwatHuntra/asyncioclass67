90e600d0deeeffc1050758833cc5f51114bbae47
[Sirirwat] <siriwat26room1@gmail.com>
2024-07-12 10:23:22 +0700

    assign5

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -4,15 +4,19 @@ from random import random
 
 async def task_coro(arg):
     value = random() +1
-    await asyncio.sleep(value)
     print(f"Microwave: ({arg}): Cooking {value} sec.")
+    await asyncio.sleep(value)
+    print(f"Microwave: ({arg}): Finish!")
+    return f"{arg} done in {value} sec."
 
 async def main():    
     tasks = ["Fried_Rice", "Noodle", "Curry"] #task list
     tasks = [asyncio.create_task(task_coro(i)) for i in tasks]
-    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
-    print("All done")
-    first = done.pop()
-    first_task = first
-    print(f"The first task is: {first_task}")
+
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    print(f"Completed {len(done)} task")        
+    for task in done:
+        print(task.result())
+    print(f"Uncomplete: {len(pending)} tasks")
+
 asyncio.run(main())
