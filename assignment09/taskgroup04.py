89384923caaf182ab20f3c5cacb2d28bf83c89a4
[Sirirwat] <siriwat26room1@gmail.com>
2024-08-23 10:26:40 +0700

    .

--- assignment09/taskgroup04.py
+++ assignment09/taskgroup04.py
@@ -17,6 +17,7 @@ async def main():
     t1 = asyncio.create_task(task1())
     t2 = asyncio.create_task(task2())
     t3 = asyncio.create_task(task3())
+    #await asyncio.sleep(0.5)
     t2.cancel()
     print(f"task1: done={t1.done()}, cancelled={t1.cancelled()}")
     print(f"task2: done={t2.done()}, cancelled={t2.cancelled()}")
