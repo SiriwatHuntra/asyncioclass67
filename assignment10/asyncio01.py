2d1635c92ff229edc6c7d8fd4f40a0a3b23d6eef
[Sirirwat] <siriwat26room1@gmail.com>
2024-09-06 08:35:43 +0700

    .

--- assignment10/asyncio01.py
+++ assignment10/asyncio01.py
@@ -1,4 +1,54 @@
 # example of using an asyncio queue
 from random import random
 import asyncio
- 
+import time
+
+#coroutine generate work
+async def producer(queue):
+    print('Producer: Running')
+    produce_time = time.perf_counter()
+    #generate work'
+    for i in range(10):
+        #generate a value
+        value = i
+        #block to simulate work
+        sleeptime = 0.99 #random()
+        print(f"> Producer {value} sleep {sleeptime}")
+        await asyncio.sleep(sleeptime)
+    #send an all done signal
+    await queue.put(None)
+    end_produce_time = time.perf_counter()
+    print(f"{time.ctime()} - Asynchronous Produce 10 tasks. Time taken: {end_produce_time-produce_time:.2f} seconds")
+    print('Producer: Done')
+
+#coroutine to cunsume work
+async def consumer(queue):
+    consume_start = time.perf_counter()
+    print('Consumer: Running')
+    #consume work
+    while True:
+        #get a unit of work without blocking
+        item = await queue.get()
+        #check for stop
+        if item is None:
+            break
+        #report
+        print(f'\t> Consumer got {item}')
+    end_consume = time.perf_counter()
+    print(f"{time.ctime()} - Asynchronous Produce 10 tasks. Time taken: {end_consume-consume_start:.2f} seconds")
+    print('Consumer: Done')
+
+#entry point coroutine
+async def main():
+    start_time = time.perf_counter()
+    #create the shared queue
+    queue = asyncio.Queue()
+    #run the producer and consumers
+    await asyncio.gather(producer(queue), consumer(queue))
+    end_time = time.perf_counter()
+    print(f"{time.ctime()} - Asynchronous get 10 tasks. Time taken: {end_time-start_time:.2f} seconds")
+
+
+#start the asyncio program
+asyncio.run(main())
+    
--- assignment10/asyncio02.py
+++ assignment10/asyncio02.py
@@ -1,16 +1,19 @@
 # example of using an asyncio queue without blocking
 from random import random
 import asyncio
+import time
  
 # coroutine to generate work
 async def producer(queue):
     print('Producer: Running')
+    produce_time = time.perf_counter()
+
     # generate work
     for i in range(10):
         # generate a value
         value = i
         # block to simulate work
-        sleeptime = random()
+        sleeptime = 0.9 #random()
         print(f"> Producer {value} sleep {sleeptime}")
         await asyncio.sleep(sleeptime)
         # add to the queue
@@ -18,11 +21,15 @@ async def producer(queue):
         await queue.put(value)
     # send an all done signal
     await queue.put(None)
+    end_produce_time = time.perf_counter()
+    print(f"{time.ctime()} - Asynchronous Produce 10 tasks. Time taken: {end_produce_time-produce_time:.2f} seconds")
     print('Producer: Done')
- 
+
 # coroutine to consume work
 async def consumer(queue):
     print('Consumer: Running')
+    consume_start = time.perf_counter()
+
     # consume work
     while True:
         # get a unit of work without blocking
@@ -30,7 +37,7 @@ async def consumer(queue):
             item = queue.get_nowait()
         except asyncio.QueueEmpty:
             print('Consumer: got nothing, waiting a while...')
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(0.8)
             continue
         # check for stop
         if item is None:
@@ -38,14 +45,20 @@ async def consumer(queue):
         # report
         print(f'\t> Consumer got {item}')
     # all done
+    end_consume = time.perf_counter()
+    print(f"{time.ctime()} - Asynchronous consume 10 tasks. Time taken: {end_consume-consume_start:.2f} seconds")
     print('Consumer: Done')
  
 # entry point coroutine
 async def main():
-    # create the shared queue
-    queue = asyncio.Queue()
-    # run the producer and consumers
+    start_time = time.perf_counter()
+    #create the shared queue
+    queue = asyncio.Queue(3)
+    #run the producer and consumers
     await asyncio.gather(producer(queue), consumer(queue))
+    end_time = time.perf_counter()
+    print(f"{time.ctime()} - Asynchronous get 10 tasks. Time taken: {end_time-start_time:.2f} seconds")
+
  
 # start the asyncio program
 asyncio.run(main())
